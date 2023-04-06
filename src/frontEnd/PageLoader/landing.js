516c0952ef7042dfd237743aaee4df995f3e259a
pendragons-code <pendragonscode@gmail.com>
2023-04-06 20:11:19 +0800

    I openedvscode by accident and some inddents and lints broke

--- src/frontEnd/PageLoader/landing.js
+++ src/frontEnd/PageLoader/landing.js
@@ -1,8 +1,10 @@
 const { quotes } = require("../../../configuration/quotes.json")
+let greet = ["ᓚᘏᗢ", "/ᐠ. ｡.ᐟ\\ ᵐᵉᵒʷˎˊ˗", "Hello World!", "Pendragon's code!", "/ᐠ - ˕ -マ Ⳋ", "/\\___/\\<br>_____(=0 - 0=)_____<br>U  U"]
 module.exports = {
 	name: "/",
 	async execute(req, res) {
 		const getRandomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-		return res.render("landingPage.ejs", { quote: getRandomQuote })
+		const getRandomGreet = greet[Math.floor(Math.random() * greet.length)]
+		return res.render("landingPage.ejs", { quote: getRandomQuote, randomGreet: getRandomGreet.replace(/ /g, "&nbsp;") })
 	}
 }
