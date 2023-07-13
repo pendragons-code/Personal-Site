bae8d3e8655bb72a4db4112ba91e50595bc547eb
pendragons-code <pendragonscode@gmail.com>
2023-07-14 01:44:31 +0800

    orrhhh

---
+++ configuration/weatherMap.js
@@ -0,0 +1,20 @@
+const weatherMapping = {
+	cloudy: {
+		icon: "fa-cloud"
+	},
+	overcast: {
+		icon: "fa-cloud"
+	},
+	rain: {
+		icon: "fa-umbrella"
+	},
+	shower: {
+		icon: "fa-umbrella"
+	},
+	storm: {
+		icon: "fa-cloud-bolt"
+	},
+	sunny: {
+		icon: "fa-cloud"
+	}
+}
--- src/loaders/weather.js
+++ src/loaders/weather.js
@@ -24,11 +24,10 @@ async function getWeather() {
 	.then(async (result) => {
 		let style = ""
 		if(result.detail) return await db.set("weather", [ currentWeather[0], currentWeather[1] ])
-		if(result.data.daily.data[0].weather.includes("cloudy") || result.data.daily.data[0].weather === "overcast") style = `<i class="fa-solid fa-cloud fa-beat-fade" style="color: #f5bc92;"> </i> ${result.data.daily.data[0].weather.replaceAll("_", " ").toUpperCase()} (weather code)`
+		if(result.data.daily.data[0].weather.includes("cloudy") || result.data.daily.data[0].weather === "overcast" || result.data.daily.data[0].weather.includes("sunny")) style = `<i class="fa-solid fa-cloud fa-beat-fade" style="color: #f5bc92;"> </i> ${result.data.daily.data[0].weather.replaceAll("_", " ").toUpperCase()} (weather code)`
 		if(result.data.daily.data[0].weather.includes("rain") || result.data.daily.data[0].weather.includes("shower")) style = `<i class="fa-solid fa-umbrella fa-beat-fade" style="color:#f5bc92;"> </i> ${result.data.daily.data[0].weather.replaceAll("_", " ").toUpperCase()} (weather code)`
 		if(result.data.daily.data[0].weather.includes("storm")) style = `<i class="fa-solid fa-cloud-bolt fa-beat-fade" style="color: #f5bc92;"> </i> ${result.data.daily.data[0].weather.replaceAll("_", " ").toUpperCase()} (weather code)`
-		if(result.data.daily.data[0].weather.includes("sunny")) style = `<i class="fa-solid fa-cloud fa-beat-fade" style="color: #f5bc92;"> </i> ${result.data.daily.data[0].weather.replaceAll("_", " ").toUpperCase()} (weather code)`
-	let newWeatherInfo = [result.data.daily.data[0], newWeatherDate, style]
+		let newWeatherInfo = [result.data.daily.data[0], newWeatherDate, style]
 		await db.set("weather", newWeatherInfo)
 	})
 }
